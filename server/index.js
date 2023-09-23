734bccad45ba2a82852b403bc15da89ee9ed4fe4
Ayush Soni <soni.ayush.2212@gmail.com>
2023-09-23 16:01:11 +0530

    Create server and client application

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
+require('dotenv').config()
+
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI);
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hi');
+})
 
 app.listen(3000, () => {
     console.log('Listening on PORT 3000');
